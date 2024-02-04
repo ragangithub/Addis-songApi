9f738124046a052594a237faed647763b461c914
Ragan <raganalemu3@gmail.com>
2024-02-04 11:37:00 +0300

    Setup server

---
+++ server.js
@@ -0,0 +1,17 @@
+// require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/songs", {
+  retryWrites: true,
+  w: "majority",
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+
+app.listen(3000, () => console.log("Server Started"));
